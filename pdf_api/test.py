49a9c9e4028d1481ccde9385c66010eadfb6b229
Rob192 <reynaud.robin@gmail.com>
2020-07-08 18:49:14 +0200

    change name of app folder

--- pdf_api/test.py
+++ pdf_api/test.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 
-text_file = Path(os.getcwd()) / 'pseudo_api' /'api_uploaded_files' / 'test.txt'
+text_file = Path(os.getcwd()) / 'pdf_api' /'api_uploaded_files' / 'test.txt'
 
 with open(text_file, 'rb') as f:
     output = f.read()
