c52deb791055f0b53a33424ea57f31e416a0e5f8
Rob192 <reynaud.robin@gmail.com>
2020-07-16 14:01:21 +0200

    update requirements.txt and Dockerfile

--- pdf_api/use_api.py
+++ pdf_api/use_api.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import os
 
 #url = "http://0.0.0.0:8000/"
-url = "http://167.172.34.189/"
+url = "http://167.172.34.189:8000/"
 
 def get_list_of_files():
     response = requests.get(f'{url}/files')
