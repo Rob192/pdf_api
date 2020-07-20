4729a687f853d760c3c02ea6a4b60bdd506374e1
Rob192 <reynaud.robin@gmail.com>
2020-07-20 15:31:32 +0200

    do not remove files for debug

--- pdf_api/use_api.py
+++ pdf_api/use_api.py
@@ -2,8 +2,8 @@ import requests
 from pathlib import Path
 import os
 
-#url = "http://0.0.0.0:8000/"
-url = "http://167.172.34.189:8000/"
+url = "http://0.0.0.0:8000/"
+# url = "http://167.172.34.189:8000/"
 
 def get_list_of_files():
     response = requests.get(f'{url}/files')
@@ -21,7 +21,9 @@ def post_file(file):
 
 if __name__ == '__main__':
     CWD = Path(os.getcwd())
+    file = CWD / 'pdf_api'/ 'test_data' / 'test.pdf'
     file = CWD / 'test_data' / 'test.pdf'
     #get_list_of_files()
-    post_file(file)
+    f = post_file(file)
+    f['text']
     #get_list_of_files()
